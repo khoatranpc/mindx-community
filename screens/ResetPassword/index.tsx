125d86b0adf54e718a8f7fa4f128e200e7a3bc3f
khotranpc <khotranpc603@gmail.com>
2024-08-23 14:57:23 +0700

    hot fix

--- screens/ResetPassword/index.tsx
+++ screens/ResetPassword/index.tsx
@@ -1,7 +1,7 @@
 'use client';
-import { Form, Flex, Input ,Button,notification} from 'antd'
+import { Form, Flex, Input, Button, notification } from 'antd'
 
-import {useState} from 'react'
+import { useState } from 'react'
 import React from 'react'
 import type { GetProps } from 'antd';
 import { useRouter } from 'next/navigation';
@@ -14,91 +14,91 @@ const index = () => {
   const [confirmPass, setConfirmPass] = useState('')
   const router = useRouter()
 
-   const onChangeOTP: OTPProps['onChange'] = (text) => {
+  const onChangeOTP: OTPProps['onChange'] = (text) => {
     console.log('onChangeOTP:', text);
   };
- const sharedProps: OTPProps = {
-    onChangeOTP,
+  const sharedProps: OTPProps = {
+    onChange: onChangeOTP,
   };
 
   const handleOnClick = () => {
     if (!email || !newPass || !confirmPass) {
-       notification.error({
+      notification.error({
         message: 'Thông báo',
-         description: 'Bạn cần điền đầy đủ tất cả các trường trước khi xác nhận!',
-         placement: 'topRight',
-         duration:4,
-        showProgress:true
-       });
-      
+        description: 'Bạn cần điền đầy đủ tất cả các trường trước khi xác nhận!',
+        placement: 'topRight',
+        duration: 4,
+        showProgress: true
+      });
+
     }
     if (newPass !== confirmPass) {
       notification.error({
         message: 'Thông báo',
         description: 'Mật khẩu nhập lại không khớp vui lòng nhập lại!',
-        placement:'topRight'
+        placement: 'topRight'
       })
     }
     else {
-        notification.success({
+      notification.success({
         message: 'Hoàn thành',
         description: 'Bạn đã cập nhật mật khẩu thành công',
-          placement: 'topRight',
-        duration:4,
-        showProgress:true
-        })
+        placement: 'topRight',
+        duration: 4,
+        showProgress: true
+      })
       // router.push(`/auth/login`);
-      
+
       console.log('email: ', email);
       console.log('newPass: ', newPass);
       console.log('confirmPass: ', confirmPass);
     }
-    
+
   }
 
   return (
     <div>
       <h3 className={'text-[2.6rem] font-bold mb-[2.8rem]'}>Reset Password</h3>
       <Form>
-      {/* nhap otp */}
+        {/* nhap otp */}
         <Form.Item className='otp-container'>
           <h4 className={'text-[2.6rem] font-bold mb-[2.8rem] mr-4'}>OTP CODE</h4>
           <Flex gap="middle" align="flex-start" vertical>
-             <Input.OTP formatter={(str) => str.toUpperCase()} {...sharedProps} />
-         </Flex>
-    </Form.Item>
+            <Input.OTP formatter={(str) => str.toUpperCase()} {...sharedProps} />
+          </Flex>
+        </Form.Item>
         {/* nhap email */}
         <Form.Item
           label="Email"
           name="email"
           rules={[
-            {required:true,message:'Bạn cần nhập email',type:'email'}
+            { required: true, message: 'Bạn cần nhập email', type: 'email' }
           ]}
-        > 
-          <Input placeholder="Nhập Email" value={ email}  onChange={(e) =>setEmail(e.target.value)}/>
+        >
+          <Input placeholder="Nhập Email" value={email} onChange={(e) => setEmail(e.target.value)} />
         </Form.Item>
         {/* nhap password va confirm password */}
-          <Form.Item
+        <Form.Item
           label="Mật khẩu mới"
           name="NewPassWord"
           rules={[
-            {required:true,message:'Bạn cần nhập mật khẩu mới'}
+            { required: true, message: 'Bạn cần nhập mật khẩu mới' }
           ]}
-        > 
-          <Input.Password placeholder="Nhập mật khẩu mới" value={ newPass} onChange={(e) =>setNewPass(e.target.value)}/>
+        >
+          <Input.Password placeholder="Nhập mật khẩu mới" value={newPass} onChange={(e) => setNewPass(e.target.value)} />
         </Form.Item>
         <Form.Item
           label="Nhập lại mật khẩu"
           name="ConfirmPassWord"
           rules={[
-            {required:true,message:'Bạn cần nhập lại mật khẩu mới'}
+            { required: true, message: 'Bạn cần nhập lại mật khẩu mới' }
           ]}
-        > 
-          <Input.Password placeholder="Nhập lại mật khẩu mới" value={ confirmPass}onChange={(e) =>setConfirmPass(e.target.value)}/>
+        >
+          <Input.Password placeholder="Nhập lại mật khẩu mới" value={confirmPass} onChange={(e) => setConfirmPass(e.target.value)} />
         </Form.Item>
         <Form.Item>
           <Button type='primary' htmlType='submit' className='w-full font-bold' onClick={handleOnClick}>
-              Xác Nhận thông tin
+            Xác Nhận thông tin
           </Button>
         </Form.Item>
       </Form>
