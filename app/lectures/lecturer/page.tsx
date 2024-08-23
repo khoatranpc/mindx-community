706311683eae36b8b5b0d6f4eca568e6cb8f1761
HaiAnh1905 <haianh198005@gmail.com>
2024-08-23 18:09:53 +0700

    Finished Design Lecturer Landingpage

--- app/lectures/lecturer/page.tsx
+++ app/lectures/lecturer/page.tsx
@@ -7,7 +7,9 @@ import { FacebookOutlined } from "@ant-design/icons";
 import { EnvironmentFilled } from "@ant-design/icons";
 import { BookOutlined } from "@ant-design/icons";
 import { ReadFilled } from "@ant-design/icons";
+import { StarFilled } from "@ant-design/icons";
 import "./style.scss";
+import { url } from "inspector";
 
 export function CoLogo() {
   return (
@@ -19,7 +21,7 @@ export function CoLogo() {
 //         <Image src={"/achievement-banner.png"} className="achiveBanner" alt="" width={1440} height={363}/>
 //     )
 // }
-const Lecturer = (props: String) => {
+const Lecturer = () => {
   return (
     <div className="teacher-pf">
       <div className="pf-banner w-fit m-auto">
@@ -34,7 +36,11 @@ const Lecturer = (props: String) => {
             Có 10 năm kinh nghiệm trong ngành lập trình phần mềm, đã có chứng
             chỉ nghiệp vụ sư phạm đạt xuất sắc
           </p>
-          <div className="full-red-box"></div>
+          <div className="full-red-box text-2xl pl-9 py-4 gap-2 flex flex-col justify-center tracking-wider">
+            <h1>Lập trình viên FullStack</h1>
+            <h1>Kỹ sư phần mềm</h1>
+            <h1>Game Developer</h1>
+          </div>
         </div>
         <div className="right">
           <div className="up-highlight">
@@ -44,15 +50,23 @@ const Lecturer = (props: String) => {
             </div>
           </div>
           <div className="down-highlight"></div>
-          <img
-            src="https://picsum.photos/362/452"
-            alt=""
+          <div
             className="teacher-img"
-          />
+            style={{
+              backgroundImage:
+                "url(https://images.squarespace-cdn.com/content/v1/54ee6b54e4b094722873774d/1651271676233-EJSOCKU9OE5ZKGALLAH4/232.jpg?format=2500w)",
+            }}
+          ></div>
           <div className="avatar-rating">
-            <img src="https://picsum.photos/158/158" className="avatar"></img>
+            <div
+              className="avatar"
+              style={{
+                backgroundImage:
+                  "url(https://images.squarespace-cdn.com/content/v1/54ee6b54e4b094722873774d/1651271676233-EJSOCKU9OE5ZKGALLAH4/232.jpg?format=2500w)",
+              }}
+            ></div>
             <div className="rating">
-              <div className="rating-score">4.0</div>
+              <div className="rating-score text-lg">4.0</div>
               <div className="rating-star">
                 <StarRating className="Star" rate={4} />
               </div>
@@ -62,7 +76,7 @@ const Lecturer = (props: String) => {
       </div>
       <div className="pf-follow">
         <div className="opacity50"></div>
-        <div className="achivement-pf">
+        <div className="achivement-pf text-4xl">
           <ul className="pf-list">
             <li>
               <div className="item">
@@ -87,25 +101,27 @@ const Lecturer = (props: String) => {
       </div>
       <div className="main">
         <div className="wrapper m-auto">
-          <img
-            src="https://picsum.photos/493/633"
-            alt=""
-            className="teacher-avatar"
-          />
-          <div className="lbl">
+          <div
+            className="picground"
+            style={{
+              backgroundImage:
+                "url(https://images.squarespace-cdn.com/content/v1/54ee6b54e4b094722873774d/1651271676233-EJSOCKU9OE5ZKGALLAH4/232.jpg?format=2500w)",
+            }}
+          ></div>
+          <div className="lbl text-3xl tracking-wide font-bold">
             <h3>THÔNG TIN GIẢNG VIÊN</h3>
           </div>
-          <div className="details ml-20 mt-72">
+          <div className="details mt-24 ml-20 flex flex-col justify-center">
             <div className="row-a pb-6 mt-5">
               <div className="field ml-7">
                 <div className="iconLeft">
                   <UserOutlined
-                    style={{ color: "#bb0409", fontSize: "4.6rem" }}
+                    style={{ color: "#bb0409", fontSize: "3.6rem" }}
                   />
                 </div>
-                <div className="text text-3xl ml-4">
+                <div className="text text-2xl ml-4">
                   <strong>Họ và tên:</strong>
-                  <div className="mt-2 small text-3xl">Nguyễn Văn A</div>
+                  <div className="mt-2 small text-2xl">Nguyễn Văn A</div>
                 </div>
               </div>
             </div>
@@ -113,12 +129,12 @@ const Lecturer = (props: String) => {
               <div className="field ml-7">
                 <div className="iconLeft">
                   <EnvironmentFilled
-                    style={{ color: "#bb0409", fontSize: "4.6rem" }}
+                    style={{ color: "#bb0409", fontSize: "3.6rem" }}
                   />
                 </div>
-                <div className="text text-3xl ml-4">
+                <div className="text text-2xl ml-4">
                   <strong>Nơi công tác: </strong>
-                  <div className="mt-2 small text-3xl">MindX Hà Nội</div>
+                  <div className="mt-2 small text-2xl">MindX Hà Nội</div>
                 </div>
               </div>
             </div>
@@ -126,12 +142,12 @@ const Lecturer = (props: String) => {
               <div className="field ml-7">
                 <div className="iconLeft">
                   <BookOutlined
-                    style={{ color: "#bb0409", fontSize: "4.6rem" }}
+                    style={{ color: "#bb0409", fontSize: "3.6rem" }}
                   />
                 </div>
-                <div className="text text-3xl ml-4">
+                <div className="text text-2xl ml-4">
                   <strong>Trình độ học vấn: </strong>
-                  <div className="mt-2 small text-3xl">Thạc sĩ</div>
+                  <div className="mt-2 small text-2xl">Thạc sĩ</div>
                 </div>
               </div>
             </div>
@@ -139,12 +155,12 @@ const Lecturer = (props: String) => {
               <div className="field ml-7">
                 <div className="iconLeft">
                   <ReadFilled
-                    style={{ color: "#bb0409", fontSize: "4.6rem" }}
+                    style={{ color: "#bb0409", fontSize: "3.6rem" }}
                   />
                 </div>
-                <div className="text text-3xl ml-4">
+                <div className="text text-2xl ml-4">
                   <strong>Môn dạy: </strong>
-                  <div className="mt-2 small text-3xl">
+                  <div className="mt-2 small text-2xl">
                     Lập trình Fullstack, Python, Java,...
                   </div>
                 </div>
@@ -154,12 +170,12 @@ const Lecturer = (props: String) => {
               <div className="field ml-7">
                 <div className="iconLeft">
                   <FacebookOutlined
-                    style={{ color: "#bb0409", fontSize: "4.6rem" }}
+                    style={{ color: "#bb0409", fontSize: "3.6rem" }}
                   />
                 </div>
-                <div className="text text-3xl ml-4">
+                <div className="text text-2xl ml-4">
                   <strong>Trang cá nhân: </strong>
-                  <div className="mt-2 small text-3xl">
+                  <div className="mt-2 small text-2xl">
                     Lập trình Fullstack, Python, Java,...
                   </div>
                 </div>
@@ -168,25 +184,80 @@ const Lecturer = (props: String) => {
           </div>
         </div>
       </div>
-      <div className="pf-story mt-9">
+      <div className="pf-story mt-12">
         <div className="story-wrapper">
           <h1 className="story-title w-fit text-4xl m-auto font-bold">
             Đôi điều về giảng viên
           </h1>
-          <div className="pic-frame">
-            <img src="https://picsum.photos/407/487" alt="" className="teacher-img" width={407} height={487}/>
-            <div className="frame-top">
-              <div className="red-box-top w-40 h-24"></div>
-              <div className="blank-box-top"></div>
-            </div>
-            <div className="frame-bot">
-              <div className="red-box-bot"></div>
-              <div className="blank-box-bot"></div>
+          <div className="main mt-8 mx-auto">
+            <div
+              className="pic-frame"
+              style={{
+                backgroundImage:
+                  "url(https://images.squarespace-cdn.com/content/v1/54ee6b54e4b094722873774d/1651271676233-EJSOCKU9OE5ZKGALLAH4/232.jpg?format=2500w)",
+              }}
+            ></div>
+            <div className="pf-content">
+              <div className="text ml-32 pt-20">
+                <h1 className="text-4xl font-bold">Đời sống</h1>
+                <p className="text-2xl">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Accusantium ex vero atque voluptates non accusamus delectus
+                  dolores nemo esse a vitae est eaque laborum nulla, dolor
+                  doloribus aliquid consequuntur voluptas. Veritatis, animi
+                  consequuntur. Ab libero natus minus maxime error rerum
+                  quibusdam dignissimos beatae porro? <br />
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Dignissimos, voluptatem eum earum rerum suscipit esse
+                  repellendus itaque exercitationem voluptatibus ut. Culpa
+                  tempore sint tempora aspernatur libero, officiis distinctio
+                  fugiat quidem nihil fuga cupiditate molestias earum, quisquam
+                  dolorem similique eligendi. Neque rerum quas nam aliquam!
+                </p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="pf-feedback"></div>
+      <div className="pf-feedback w-full mt-28">
+        <div className="wrapper w-full flex flex-col items-center justify-center">
+          <div className="header mb-10 flex flex-col items-center">
+            <div className="feedback-title text-3xl font-bold ">
+              Những điều học sinh nói
+            </div>
+            <div className="line mt-6 flex items-center justify-center gap-8">
+              <div className="left"></div>
+              <div className="middle">
+                <StarFilled style={{ fontSize: "1.5rem", color: "#bb0409" }} />
+              </div>
+              <div className="right"></div>
+            </div>
+          </div>
+          <div className="student-feedback gap-4 text-justify tracking-wide leading-6 text-2xl flex flex-col items-center justify-center">
+            <div className="body gap-4 flex flex-col items-center justify-center">
+              <div className="comment font-bold ">Thầy dạy rất tận tâm</div>
+              <div className="content">
+                “Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Curabitur pretium dui sed urna rhoncus porttitor. Donec
+                ultricies tellus ex, sed finibus nisi fermentum non. Etiam vel
+                pulvinar neque. Maecenas lacinia mi ut nisl auctor lacinia.
+                Mauris tristique nisi vel massa mattis sollicitudin. Maecenas
+                urna purus, tempor non ex vel, malesuada lacinia lorem.”
+                <strong> - Nhữ Nguyên</strong>
+              </div>
+              <div className="line flex items-center justify-center gap-8">
+                <div className="left"></div>
+                <div className="middle">
+                  <StarFilled
+                    style={{ fontSize: "1.5rem", color: "#bb0409" }}
+                  />
+                </div>
+                <div className="right"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
